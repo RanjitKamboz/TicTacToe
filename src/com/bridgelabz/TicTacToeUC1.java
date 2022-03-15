2c50cedfc9ecb47b270fecf03155f266e919e063
Ranjit Singh <ranjitkamboz302@gmail.com>
2022-03-15 20:00:38 +0530

    Add UC-1

---
+++ src/com/bridgelabz/TicTacToeUC1.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class TicTacToeUC1 {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe Game !!! :)");
+        System.out.println("Please Enter Your name : ");
+        String playerName = scanner.nextLine();
+        char[] board = new char[10];
+    }
+}
